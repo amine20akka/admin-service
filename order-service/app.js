dd6bbd87f551998756d3e116999e83f61dc6cb64
waelabdallah <waelabdallah@callem.ai>
2024-12-31 07:47:12 +0100

    ajouter prometheus

--- order-service/app.js
+++ order-service/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 // const axios = require('axios');
 const dapr = require('@dapr/dapr');
-
+const client1 = require('prom-client');
 const daprHost = 'http://localhost'; // Dapr Sidecar Host (not used for Axios)
 const daprPort = '3500'; // Dapr HTTP Port (not used for Axios)
 const STATE_STORE_NAME = 'mongodb-state-store'; // State store component name
@@ -26,6 +26,12 @@ const OrderSchema = new mongoose.Schema({
 });
 
 const Order = mongoose.model('Order', OrderSchema);
+
+
+const register = new client1.Registry();
+
+// Collecte les métriques par défaut (CPU, mémoire, etc.)
+client1.collectDefaultMetrics({ register });
 
 // Create a new order and call Customer and Product services
 app.post('/order', async (req, res) => {
@@ -101,6 +107,14 @@ app.get('/order/:id', async (req, res) => {
     }
 });
 
+
+// Endpoint pour exposer vos métriques
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+  });
+ 
+  
 app.listen(5002, () => {
     console.log('Order service is running on port 5002');
 });
